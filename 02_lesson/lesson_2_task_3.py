b826cd51215fd991a994518fecbb09bf38fa37d0
My Name Irina <irinaivanova19711988@gmail.com>
2025-06-11 12:07:28 +0700

    lesson 2

---
+++ 02_lesson/lesson_2_task_3.py
@@ -0,0 +1,6 @@
+import math
+def sguare(x):
+    return math.ceil(x*x)
+x = float(input())
+print(f'Площадь квадрата{math.ceil(x*x)}')
+    
---
+++ 02_lesson/lesson_2_task_5.py
@@ -0,0 +1,16 @@
+def month_to_season(month):
+    if  1 <= month <= 2 and 12 == month:
+        return "зима"
+    elif 6 <= month <= 8:
+        return "лето"
+    elif 9 <= month <= 11:
+        return "осень"
+    elif 3 <= month <= 5:
+        return "весна"
+    else:
+        return "Неверный номер месяца"
+    
+month = int(input("Введите номер месяца(1-12):"))
+season = month_to_season(month)
+    
+print(f"Месяц {month} относится к сезону: {season}.")
