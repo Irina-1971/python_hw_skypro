23c609a82b4ecc6d2906a8fda6dadb2b42a05f1f
My Name Irina <irinaivanova19711988@gmail.com>
2025-08-18 22:08:58 +0700

    08-fix

---
+++ 08-fix_lesson/test_negative.py
@@ -0,0 +1,49 @@
+import pytest
+import requests
+
+BASE_URL = "https://yougile.com/api-v2"
+
+Key = ""
+headers = {
+"Authorization": Key,
+"Content-Type": "application/json"
+}
+
+
+def test_create_company():
+    payload={
+"title": ""
+}
+    
+    response = requests.post(BASE_URL+'/projects', headers=headers, json=payload)
+
+    assert response.status_code == 400
+
+
+
+
+def test_get_negativ():
+    responce = requests.get(BASE_URL + '/projects/' + '1', headers=headers)
+   
+    assert responce.status_code == 404
+
+
+
+def test_edit_company():
+    payload={
+    "title": "home1"
+}
+
+    
+    response = requests.post(BASE_URL+'/projects', headers=headers, json=payload)
+    project_id = response.json()["id"]
+    payload={
+        "title": "home2"
+    }
+
+    response = requests.put(BASE_URL+'/projects'+project_id, headers=headers, json=payload)
+
+    assert response.status_code == 400
+    response = requests.get(BASE_URL+'/projects'+project_id +1, headers=headers)
+    data = response.json()
+    assert data["title"] == "home2"
---
+++ 09_lesson/test_database.py
@@ -0,0 +1,4 @@
+import pytest
+from sqlalchemy import create_engine, text
+
+db_connection_string = 
