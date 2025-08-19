a2d96b23a4fe5b052ed10dadb554692e132b3324
My Name Irina <irinaivanova19711988@gmail.com>
2025-08-20 00:58:35 +0700

    Update test_data.py for lesson9

--- 09_lesson/test_data.py
+++ 09_lesson/test_data.py
@@ -1,12 +1,55 @@
-from sqlalchemy import create_engine, inspect
+from sqlalchemy import create_engine, text
+import psycopg2
+import pytest
 
 
-db_connection_string = "postgresql://qa:skyqa@5.101.50.27:5432/x_clients"
+db_connection_string = "postgresql://postgres:123@localhost:5432/"
 db = create_engine(db_connection_string)
 
 
-def test_db_connection():
-# Используем инспектор для получения информации о таблицах
-	inspector = inspect(db)
-	names = inspector.get_table_names()
-	assert names[1] == 'app_users'
+def test_create_students():
+    connection=db.connect()
+    connection.execute("select * from student")
+    
+def test_searsh_students():
+    result = db.execute(text("SELECT * FROM student"))	
+    
+ 
+    
+
+def test_create_subjects():
+    connection = db.connect()
+    res=connection.execute("select count(*) from subject").fetchone()[0]
+
+    result=  connection.execute("INSERT INTO subject (subject_id, subject_title) values (17, 'Football')")
+    res2 = connection.execute("select count(*) from subject").fetchone()[0]
+    assert res==res2-1
+
+
+def test_update_subject_name():
+    connection = db.connect()
+    initial_count = connection.execute("SELECT COUNT(*) FROM subject").fetchone()[0]
+    new_subject_id = 18
+    new_subject_title = 'Baseball'
+    connection.execute(
+        f""" INSERT INTO subject (subject_id, subject_title) VALUES ({new_subject_id}, '{new_subject_title}') """)
+
+    fetched_subject = connection.execute(
+        f"SELECT subject_title FROM subject WHERE subject_id={new_subject_id}"
+    ).fetchone()
+    assert fetched_subject[0] == new_subject_title
+
+def test_delete_subject():
+    connection = db.connect()
+    initial_count = connection.execute("SELECT COUNT(*) FROM subject").fetchone()[0]
+    subject_to_delete_id = 13
+    connection.execute(f"DELETE FROM subject WHERE subject_id={subject_to_delete_id}")
+    final_count = connection.execute("SELECT COUNT(*) FROM subject").fetchone()[0]
+    assert final_count == initial_count - 1
+    deleted_subject = connection.execute(
+        f"SELECT * FROM subject WHERE subject_id={subject_to_delete_id}"
+    ).fetchone()
+    assert deleted_subject is None
+    
+
+
